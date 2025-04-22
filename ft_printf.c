c22a8f233422cdc79c773048df2563ad4a5bd803
dylina <109595463+dylina@users.noreply.github.com>
2025-04-22 18:25:33 +0200

    Add files via upload

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: dgorceac <dgorceac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 16:21:43 by dgorceac          #+#    #+#             */
-/*   Updated: 2025/04/18 16:21:57 by dgorceac         ###   ########.fr       */
+/*   Updated: 2025/04/22 17:44:28 by dgorceac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,23 @@
 
 static int	check_format(char str, va_list args)
 {
-	int i;
-
-	i = 0;
 	if (str == 'c')
 		write (1, (char[]){va_arg(args, int)}, 1);
 	else if (str == 's')
-		write (1, (char[]){va_arg(args, int)}, 1);
-	// else if (str == 'p')
-	// else if (str == 'd')
-	// else if (str == 'i')
-	// else if (str == 'u')
-	// else if (str == 'x')
-	// else if (str == 'X')
-	// else if (str == '%')
+		ft_str_fd(va_arg(args, char*), 1);
+	else if (str == 'd' || str == 'i')
+		ft_nbr_fd(va_arg(args, int), 1);
+	else if (str == 'u')
+		ft_unsigned(va_arg(args, int));
+	else if (str == 'p')
+		ft_hex_ptr(va_arg(args, int));
 
+	// else if (str == 'x')
+
+	// else if (str == 'X')
+
+	else if (str == '%')
+		write(1, &str, 1);
 }
 
 int	ft_printf(const char *str, ...)
@@ -62,6 +64,9 @@ int	ft_printf(const char *str, ...)
 }
 int	main(void)
 {
-	ft_printf("am :%d ani si ma numesc :%s", 1, "Dumi");
+	int x = 10;
+	int *p = &x;
+
+	ft_printf("am :%d ani %% si luni %i si ma numesc :%s anul: %u si %u adresa:%p\n", 1, 55, "Dumi", 000, 215, (void *)&x);
 	return (0);
 }
