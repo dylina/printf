5411e644844feaae8352a4ed7ec327183eca9a79
dylina <109595463+dylina@users.noreply.github.com>
2025-04-22 18:26:05 +0200

    Add files via upload

--- printf.h
+++ printf.h
@@ -6,7 +6,7 @@
 /*   By: dgorceac <dgorceac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 16:29:52 by dgorceac          #+#    #+#             */
-/*   Updated: 2025/04/18 14:50:02 by dgorceac         ###   ########.fr       */
+/*   Updated: 2025/04/22 17:44:17 by dgorceac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,9 @@
 
 int	ft_printf(const char *str, ...);
 int	ft_putchar(char c);
-
+int	ft_nbr_fd(int n, int fd);
+int	ft_str_fd(char *s, int fd);
+int	ft_unsigned(unsigned int n);
+int	ft_hex_ptr(int ptr);
 
 #endif
