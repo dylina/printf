fd5073aa9c92340058d1800b4f15ba91248cc20a
dylina <109595463+dylina@users.noreply.github.com>
2025-04-23 16:47:13 +0200

    Add files via upload

--- ft_printf.c
+++ ft_printf.c
@@ -6,43 +6,44 @@
 /*   By: dgorceac <dgorceac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 16:21:43 by dgorceac          #+#    #+#             */
-/*   Updated: 2025/04/22 17:44:28 by dgorceac         ###   ########.fr       */
+/*   Updated: 2025/04/23 16:44:28 by dgorceac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "printf.h"
-#include <stdio.h>
+#include "ft_printf.h"
 
 static int	check_format(char str, va_list args)
 {
 	if (str == 'c')
-		write (1, (char[]){va_arg(args, int)}, 1);
+		return (ft_putchar(va_arg(args, int)));
 	else if (str == 's')
-		ft_str_fd(va_arg(args, char*), 1);
+		return (ft_str_fd(va_arg(args, char *), 1));
 	else if (str == 'd' || str == 'i')
-		ft_nbr_fd(va_arg(args, int), 1);
+		return (ft_nbr_fd(va_arg(args, int), 1));
 	else if (str == 'u')
-		ft_unsigned(va_arg(args, int));
+		return (ft_unsigned(va_arg(args, int)));
 	else if (str == 'p')
-		ft_hex_ptr(va_arg(args, int));
-
-	// else if (str == 'x')
-
-	// else if (str == 'X')
-
+		return (ft_hex_ptr(va_arg(args, unsigned long long)));
+	else if (str == 'x')
+		return (ft_hex_min_ptr(va_arg(args, unsigned int)));
+	else if (str == 'X')
+		return (ft_hex_maj_ptr(va_arg(args, unsigned int)));
 	else if (str == '%')
-		write(1, &str, 1);
+		return (write(1, "%", 1));
+	return (check_format(str, args));
 }
 
 int	ft_printf(const char *str, ...)
 {
-	int	i;
-	int	count;
-	va_list args;
+	int		i;
+	int		count;
+	va_list	args;
 
 	i = 0;
 	count = 0;
 	va_start(args, str);
+	if (!str)
+		return (-1);
 	while (str[i])
 	{
 		if (str[i] == '%')
@@ -55,18 +56,24 @@ int	ft_printf(const char *str, ...)
 			write(1, &str[i], 1);
 			count++;
 		}
-	i++;
+		i++;
 	}
-	va_arg(args, char *);
 	va_end(args);
-	printf("count: %d\n", count);
 	return (count);
 }
-int	main(void)
-{
-	int x = 10;
-	int *p = &x;
 
-	ft_printf("am :%d ani %% si luni %i si ma numesc :%s anul: %u si %u adresa:%p\n", 1, 55, "Dumi", 000, 215, (void *)&x);
-	return (0);
-}
+// int	main(void)
+// {
+// 	int x = 10;
+// 	int *p = &x;
+
+// 	ft_printf("d: %d\n", 25);
+// 	ft_printf("%: %%\n");
+// 	ft_printf("I: %i\n", 55);
+// 	ft_printf("S: %s\n", "Dumi");
+// 	ft_printf("U: %u si %u\n", 0, 215);
+// 	ft_printf("p: %p\n", (void *)&x);
+// 	ft_printf("Hexa lowercase: %x\n", 3735928559u);
+// 	ft_printf("Hexa uppercase: %X\n", 3735928559u);
+// 	return (0);
+// }
